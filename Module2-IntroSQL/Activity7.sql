6f03f536f4169d1f54b34eed25871a51b9594925
Dot Silverman <dot.silverman10@gmail.com>
2022-04-16 22:06:18 -0500

    Update Activity7.sql
    Unit 7 focused on CASE statements, creating views, and stored procedures. The three activities 
    below are based on the fictitious island Basura and the mysql world database.

--- Module2-IntroSQL/Activity7.sql
+++ Module2-IntroSQL/Activity7.sql
@@ -1,8 +1,33 @@
+/* INFO: Unit 7 focused on CASE statements, creating views, and stored procedures. The three activities 
+below are based on the ficticious island Basura and the mysql world databse.*/
+
+# activity 2.7.1 
+# task 1 - create a list of languages that are spoken by more than 10% of any country's poulation 
+SELECT language FROM countrylanguage 
+WHERE percentage > 10.0;
+
+# task 2 - create a list of cities in the United States with populations 
+# greater than one million
+SELECT name FROM city 
+WHERE countrycode = (SELECT code FROM country WHERE Name = 'United States')
+AND population > 1000000;
+-- -----------------------------------------------------------------------------------------
+# activity 2.7.2 
+# task 1 - create a view that lists every country with a surface area of less than 1500 sq/km
+CREATE VIEW  tinycountries AS
+SELECT name, surfacearea FROM country 
+WHERE surfacearea < 1500; 
+
+# task 2 - create a view that lists all countries that are federal republics 
+CREATE VIEW federalrepublics_list AS 
+SELECT code, name, population, headofstate, language FROM country 
+JOIN countrylanguage 
+ON country.code = countrylanguage.countrycode 
+WHERE governmentform = 'Federal Republic';
+
+-- -----------------------------------------------------------------------------------------
 # activity 2.7.3 
-
-# task 3 - create a procedure to display the following information about any user-defined
-# country: code, name, continent, population, head of state 
-
+# task 1 - create a procedure to display: code, name, continent, population, head of state 
 DELIMITER $ 
 CREATE PROCEDURE worLd.turtleman_check(IN user_country varchar(255)) 
 BEGIN
